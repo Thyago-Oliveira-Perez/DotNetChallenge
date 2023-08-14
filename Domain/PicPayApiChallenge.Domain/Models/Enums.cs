09140621186a985410629b8bc3a7d177ed4094d9
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-08-14 13:40:31 -0300

    feat: add KeyType enum

---
+++ Domain/PicPayApiChallenge.Domain/Models/Enums.cs
@@ -0,0 +1,13 @@
+﻿namespace PicPayApiChallenge.Domain.Models
+{
+    public class Enums
+    {
+        public enum KeyType
+        {
+            Email = 1,
+            Cpf = 2,
+            RandomKey = 3,
+            Phone = 4
+        }
+    }
+}
