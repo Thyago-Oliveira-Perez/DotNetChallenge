36777efc357c4f8686bd4b83ad69e8a751676493
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-08-19 08:32:09 -0300

    feat: add Context class to connect with the database

--- Data/PicPayApiChallenge.Data/Context/Context.cs
+++ Data/PicPayApiChallenge.Data/Context/Context.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using PicPayApiChallenge.Domain.Models;
 
 namespace PicPayApiChallenge.Data.Context
 {
     public class Context : DbContext
     {
+        public Context(DbContextOptions options) : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        public DbSet<TransactionEntity> Transactions { get; set; }
     }
 }
--- Domain/PicPayApiChallenge.Domain/Models/UserEntity.cs
+++ Domain/PicPayApiChallenge.Domain/Models/UserEntity.cs
@@ -6,7 +6,7 @@ namespace PicPayApiChallenge.Domain.Models
     {
         public string? Name { get; set; }
         public string? Email { get; set; }
-        public string CPF { get; set; }
+        public string? CPF { get; set; }
         public string Password { get; set; }
         public UserType UserType { get; set; }
     }
