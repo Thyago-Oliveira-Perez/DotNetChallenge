10d0c4d06f92a76697367216856d6f8c8622dfaa
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-10-28 10:53:21 -0300

    feat: add TransferController and remove KeyController

---
+++ Api/PicPayApiChallenge.API/Controllers/TransferController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PicPayApiChallenge.API.Controllers
+{
+    [ApiController]
+    [Route("keys")]
+    public class TransferController : Controller
+    {
+        private readonly ILogger<TransferController> _logger;
+
+        public TransferController(ILogger<TransferController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult SendPix(int id, IFormCollection collection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
